4daf6bf57f73c2536867c03306beddee8f3f1520
RobunGid <robungidorig@gmail.com>
2025-11-23 20:18:15 +0300

    templates improvements

--- DJCRM/teams/context_processors.py
+++ DJCRM/teams/context_processors.py
@@ -1,5 +1,8 @@
 from teams.models import Team
 
 def team(request):
-    team = Team.objects.filter(created_by=request.user).first()
+    if request.user.is_authenticated:
+	    team = Team.objects.filter(created_by=request.user).first()
+    else:
+        team = None
     return {"team": team}
