ac821c967520415a9f500ae0be3c308152367319
RobunGid <robungidorig@gmail.com>
2025-12-10 18:03:46 +0300

    add message min length

--- DJCRM/chat/models.py
+++ DJCRM/chat/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinLengthValidator
 
 class Message(models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="sender", on_delete=models.CASCADE)
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="receiver", on_delete=models.CASCADE)
-    text = models.CharField(4096)
+    text = models.TextField(max_length=1024, validators=(MinLengthValidator(1),))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ("created_at",)
     
     def __str__(self):
         sender = getattr(self.sender, "user", None)
--- DJCRM/chat/views.py
+++ DJCRM/chat/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.views import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.safestring import SafeString
+from django.db.models import Q
+
+from chat.models import Message
 
 class ChatView(LoginRequiredMixin, TemplateView):
     template_name = "chat/chat.html"
@@ -9,5 +12,11 @@ class ChatView(LoginRequiredMixin, TemplateView):
         context["sender"] = SafeString({
             "username": self.request.user.username
         })
+        context["chat_messages"] = Message.objects.filter(
+            Q(sender=self.request.user) & Q(receiver__pk=self.kwargs["user_pk"])
+            |
+            Q(sender__pk=self.kwargs["user_pk"]) & Q(receiver=self.request.user)
+            
+            )
         return context
     
