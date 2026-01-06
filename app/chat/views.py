a28da2ddd8b0fe106855e1313e8ced17a233b215
RobunGid <robungidorig@gmail.com>
2026-01-06 16:10:25 +0300

    add fullname in chat

--- app/chat/views.py
+++ app/chat/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.views import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.utils.safestring import SafeString
 from django.db.models import Q
 
 from chat.models import Message
@@ -9,14 +8,10 @@ class ChatView(LoginRequiredMixin, TemplateView):
     template_name = "chat/chat.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["sender"] = SafeString({
-            "username": self.request.user.username
-        })
+        context["sender"] = self.request.user
         context["chat_messages"] = Message.objects.filter(
-            Q(sender=self.request.user) & Q(receiver__pk=self.kwargs["user_pk"])
-            |
+            Q(sender=self.request.user) & Q(receiver__pk=self.kwargs["user_pk"]) |
             Q(sender__pk=self.kwargs["user_pk"]) & Q(receiver=self.request.user)
-            
             )
         return context
     
